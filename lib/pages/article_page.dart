31a724b090428e2ca45bdd312cbe3563baed3754
oussamaHJM <oussama.elhajjam1997@gmail.com>
2020-11-06 17:57:30 +0100

    Setting up MobX Architecture :heavy_check_mark:

---
+++ lib/pages/article_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('lol'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:ithea/store/counter/counter.dart';
+
+class CounterPage extends StatelessWidget {
+  CounterPage({Key key}) : super(key: key);
+
+  final Counter counter = Counter();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mobx'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+                'Counter',
+                style: TextStyle(fontSize: 30),
+            ),
+            Observer(
+                builder: (_) => Text(
+                  '${counter.count}',
+                ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: counter.increment,
+                    icon: const Icon(Icons.add),
+                    label: const Text('Add')),
+                FlatButton.icon(
+                    onPressed: counter.decrement,
+                    icon: const Icon(Icons.remove),
+                    label: const Text('Minus')),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
