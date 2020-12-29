e7b076634d31f29b7c206b499694bcf656436b90
oussamaHJM <oussama.elhajjam1997@gmail.com>
2020-12-29 18:40:02 +0100

    Article Page and Article details page done
    merge

--- lib/pages/article_page.dart
+++ lib/pages/article_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+import 'article_details_page.dart';
+
+
 class ArticlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -35,14 +38,6 @@ class ArticlePage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.only(left: 20),
         children: <Widget>[
-          const Text(
-            'Boutique',
-            style: TextStyle(
-              fontFamily: 'Varela',
-              fontSize: 42,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
           Container(
             padding: const EdgeInsets.only(right: 15),
             width: MediaQuery.of(context).size.width - 30,
@@ -54,18 +49,18 @@ class ArticlePage extends StatelessWidget {
               mainAxisSpacing: 10,
               childAspectRatio: 0.8,
               children: <Widget>[
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png',
-                    false, context),
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png', true,
-                    context),
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png', true,
-                    context),
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png',
-                    false, context),
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png',
-                    false, context),
-                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic.png',
-                    false, context),
+                _buildCard('Tea1', '14,00 €', 'assets/images/teaPic.png',
+                    false, context, false),
+                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic2.png', true,
+                    context, false),
+                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic3.png', true,
+                    context, false),
+                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic4.png',
+                    false, context, false),
+                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic5.png',
+                    false, context, false),
+                _buildCard('Tea1', '3.99 USD', 'assets/images/teaPic6.png',
+                    false, context, false),
               ],
             ),
           ),
@@ -74,70 +69,110 @@ class ArticlePage extends StatelessWidget {
     );
   }
 
-  Widget _buildCard(
-      String name, String price, String imgpath, bool isFavourite, context) {
+  Widget _buildCard(String name, String price, String imgpath, bool isFavourite,
+      context, bool added) {
     return Padding(
-      padding: const EdgeInsets.only(top: 15, bottom: 5, left: 5, right: 5),
-      child: InkWell(
-        onTap: () {},
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 3,
-                blurRadius: 5,
-              )
-            ],
-            color: Colors.white,
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    if (isFavourite) const Icon(Icons.favorite_outlined, color: Colors.red) else const Icon(Icons.favorite_outline, color: Colors.red)
-                  ],
+        padding: const EdgeInsets.only(top: 15, bottom: 5, left: 5, right: 5),
+        child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ArticleDetail(
+                    assetPath: imgpath,
+                    teaprice: price,
+                    teaname: name,
+                  )));
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                  )
+                ],
+                color: Colors.white,
+              ),
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      if (isFavourite)
+                        const Icon(Icons.favorite_outlined, color: Colors.red)
+                      else
+                        const Icon
+                          (Icons.favorite_outline, color: Colors.red)
+                    ],
+                  ),
                 ),
-              ),
-              Hero(
-                tag: imgpath,
-                child: Container(
-                  height: 75,
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                    image: AssetImage(
-                      imgpath,
-                    ),
-                  )),
+                Hero(
+                  tag: imgpath,
+                  child: Container(
+                    height: 75,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage(
+                        imgpath,
+                      ),
+                    )),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 7,
-              ),
-              Text(
-                price,
-                style: const TextStyle(
-                  color: Colors.greenAccent,
-                  fontFamily: 'Varela',
-                  fontSize: 14,
+                const SizedBox(
+                  height: 7,
                 ),
-              ),
-              Text(
-                name,
-                style: const TextStyle(
-                  color: Colors.black45,
-                  fontFamily: 'Varela',
-                  fontSize: 14,
+                Text(
+                  price,
+                  style: const TextStyle(
+                    color: Colors.greenAccent,
+                    fontFamily: 'Varela',
+                    fontSize: 14,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+                Text(
+                  name,
+                  style: const TextStyle(
+                    color: Colors.black45,
+                    fontFamily: 'Varela',
+                    fontSize: 14,
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(8),
+                    child:
+                        Container(color: const Color(0xFFEBEBEB), height: 1)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 5, right: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      if (!added) ...[
+                        const Icon(Icons.shopping_basket,
+                            color: Colors.greenAccent, size: 12.0),
+                        const Text('Add to cart',
+                            style: TextStyle(
+                                fontFamily: 'Varela',
+                                color: Colors.greenAccent,
+                                fontSize: 12))
+                      ],
+                      if (added) ...[
+                        const Icon(Icons.remove_circle_outline,
+                            color: Colors.greenAccent, size: 12.0),
+                        const Text('3',
+                            style: TextStyle(
+                                fontFamily: 'Varela',
+                                color: Colors.greenAccent,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12.0)),
+                        const Icon(Icons.add_circle_outline,
+                            color: Colors.greenAccent, size: 12.0),
+                      ]
+                    ],
+                  ),
+                ),
+              ]),
+            )));
   }
 }
