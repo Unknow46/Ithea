166267cb689d65c8512a6b8bd284bbaa0e3af9a3
zaydane <zaydane@betmanagement.eu>
2020-12-29 15:41:25 +0100

    minor fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,10 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ithea/main.dart';
+import 'package:ithea/pages/counter_page.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(CounterPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
