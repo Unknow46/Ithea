7659ef104753d36bdc30cae66644948eac60ccd2
oussamaHJM <oussama.elhajjam1997@gmail.com>
2020-12-29 19:04:33 +0100

    coorections of the article page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ithea/pages/article_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ithea/pages/authentication/landing_screen.dart';
-import 'package:ithea/pages/authentication/login.dart';
 
+// ignore: avoid_void_async
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -24,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ArticlePage(),
+      home: const ArticlePage(),
     );
   }
 }
